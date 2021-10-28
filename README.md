4fd614ae633e71027298a88e7bb542a5339e7164
GMYS Jan <jan.gmys@inria.fr>
2021-10-28 23:12:46 +0200

    simpleGA_ONEMAX example

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # PyParadisEO
 
 A Python Framework for Metaheuristics
+
+## Installation instructions
+
+The following has been tested on
+- Ubuntu 18.04.2 LTS
+- cmake 3.21.3
+- boost version 1.65.1
+- python 3.6.13
+- scikit-build==0.12.0
+- numpy==1.19.5
+- pytest==6.2.5
+
+We're aware that there might be installation issues on different systems. In future versions this will hopefully become much easier.
+
+1. clone this repository. For example :
+
+`git clone git@gitlab.inria.fr:paradiseo/pyparadiseo.git`
+
+
+2. In the `pyparadiseo` root directory (i.e. where setup.py is located) :
+
+`python -m pip install .`
+
+Note: this may create a directory `_skbuild`. If the installation fails, this directory should be deleted before you re-attempt to install pyparadiseo.
+
+
+3. Now run the tests
+
+`python -m pytest tests`
+
+## Quick start : GA - Max One
+
+TODO ::
+encoding
+
+from encoding import binary_vector
+
+binary_vector.initializer()
+
+binary_vector.mutation.bitFlip()
+
+binary_vector.crossover.onePt()
+
+
+
+problem
+
+from problems import oneMax
---
+++ examples/simpleGA_OneMax.py
@@ -0,0 +1,48 @@
+import pyparadiseo as pp
+
+from pyparadiseo import eo
+from pyparadiseo.eo import selector,continuator,algo
+
+import copy
+import numpy as np
+
+def mut(ind) -> bool:
+    #choose random bits to flip with 5% proba
+    flip = np.random.choice([0, 1], size=len(ind), p=[.95, .05])
+
+    for e,i in enumerate(flip):
+        if i:
+            ind[e] = 1 - ind[e]
+    return True
+
+
+def cross(ind1,ind2):
+    tmp = copy.deepcopy(ind1)
+
+    point = np.random.randint(len(ind1))
+
+    ind1[point:] = ind2[point:]
+    ind2[point:] = tmp[point:]
+
+    return True
+
+
+if __name__ == "__main__":
+    myinit = pp.Init(lambda : np.random.randint(0,2,16))
+    myeval = pp.FitnessEval(lambda sol: np.sum(sol))
+
+    sga = eo.algo.SGA(
+        selector.DetTournamentSelect(),
+        pp.pyQuadOp(cross),0.2,
+        pp.pyMonOp(mut),0.5,
+        pp.FitnessEval(lambda sol: np.sum(sol)),
+        continuator.GenContinue(100)
+    )
+
+    pop = pp.Pop(20, myinit)
+    for ind in pop:
+        myeval(ind)
+
+    # print(pop)
+    sga(pop)
+    print(pop.best())
