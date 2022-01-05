924a8cca14e3209b0508d88af6b0ea2f6eb4f365
GMYS Jan <jan.gmys@inria.fr>
2022-01-05 19:38:51 +0100

    import EvalContinue

--- src/pyparadiseo/eo/continuator/__init__.py
+++ src/pyparadiseo/eo/continuator/__init__.py
@@ -1,6 +1,8 @@
 # move to eval?
 from ..._core import eoEvalFuncCounter as EvalFuncCounter
 
+from ..._core import eoEvalContinue as EvalContinue
+
 from ..._core import eoGenContinue as GenContinue
 from ..._core import eoCombinedContinue as CombinedContinue
 from ..._core import eoSteadyFitContinue as SteadyFitContinue
