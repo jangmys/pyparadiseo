708aa1995f4fd2ac5a28c531da16c62c3a5b4486
GMYS Jan <jan.gmys@inria.fr>
2022-01-10 17:32:11 +0100

    import TruncatedSelectMany

--- src/pyparadiseo/eo/selector/__init__.py
+++ src/pyparadiseo/eo/selector/__init__.py
@@ -4,7 +4,7 @@ from ...core import eoSelectMany as SelectMany
 from ...core import eoSelectNumber as SelectNumber
 from ...core import eoSelectPerc as SelectPerc
 from ...core import eoTruncSelect as TruncSelect
-from ...core import eoTruncatedSelectMany as TruncatedSelectOne
+from ...core import eoTruncatedSelectMany as TruncatedSelectMany
 # from selectOne.cpp
 from ...core import eoSelectOne as SelectOne
 from ...core import eoDetTournamentSelect as DetTournamentSelect
