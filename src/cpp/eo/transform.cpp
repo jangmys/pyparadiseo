e2ca986267a42014014388b3149ff8be24764c5a
GMYS Jan <jan.gmys@inria.fr>
2022-01-10 17:31:38 +0100

    test boost python doc on SGATransform

--- src/cpp/eo/transform.cpp
+++ src/cpp/eo/transform.cpp
@@ -18,16 +18,29 @@ transform()
 
     class_<eoSGATransform<PyEOT>, bases<eoTransform<PyEOT> > >
         ("eoSGATransform",
-        "SGADOCTRING",
+        "Simple GA Transform\n\n"
+        "Transforms a population by successive application of crossover\n"
+        "and mutation operators\n",
         init<
             eoQuadOp<PyEOT>&,
             double,
             eoMonOp<PyEOT>&,
             double
-        >()
+        >((arg("self"),arg("_cross"),arg("_cProba"),arg("_mutate"),arg("_mProba")),
+        "Start the Foo.\n\n"
+        ":param _cross: a crossover\n"
+        ":type _cross: eoQuadOp\n"
+        ":param _cProba: crossover probabilty\n"
+        ":type _cProba: float\n"
+        ":param _mutate: mutation operator\n"
+        ":type _mutate: eoMonOp\n"
+        ":param _mProba: mutation probabilty\n"
+        ":type _mProba: float\n"
+        )
         )
         .def("__call__", &eoSGATransform<PyEOT>::operator (),
-        "operator docstring"
+        (arg("_pop")),
+        "applies _mutate and _cross operators on _pop"
         )
         ;
     //
