95f02e26f776854ef57c96b9694a37b66014d255
GMYS Jan <jan.gmys@inria.fr>
2022-01-28 13:37:52 +0100

    add bounds

--- src/cpp/eo/bounds.cpp
+++ src/cpp/eo/bounds.cpp
@@ -1,12 +1,16 @@
 #include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
 #include <utils/eoRealBounds.h>
 #include <utils/eoRealVectorBounds.h>
 
-using namespace boost::python;
+namespace p = boost::python;
+namespace np = boost::python::numpy;
 
-struct eoRealBoundsWrap : eoRealBounds, wrapper<eoRealBounds> {
+
+
+struct eoRealBoundsWrap : eoRealBounds, p::wrapper<eoRealBounds> {
 public:
     // pure virtuals
     bool
@@ -106,20 +110,148 @@ public:
 };
 
 
+//wrapper (needed because of immutable scalar arguments in python)
+double foldsInBoundsWrap(eoRealBounds &real_bounds, double val) {
+    real_bounds.foldsInBounds(val);
+    return val;
+};
+
+double truncateWrap(eoRealBounds& real_bounds, double val) {
+    real_bounds.truncate(val);
+    return val;
+}
+
+//the same
+double vec_foldsInBoundsWrap(eoRealVectorBounds &real_vec_bounds, unsigned i, double val) {
+    real_vec_bounds[i]->foldsInBounds(val);
+    return val;
+};
+
+//converter works only for rvalues
+//copy code ... easier than converting ndarray to std::vector and back
+void vec_foldsInBoundsWrap2(eoRealVectorBounds &real_vec_bounds, np::ndarray vec) {
+    double* arr = reinterpret_cast<double*>(vec.get_data());
+
+    for(unsigned i =0;i<real_vec_bounds.size();i++){
+        real_vec_bounds[i]->foldsInBounds(arr[i]);
+    }
+};
+
+double vec_truncateWrap(eoRealVectorBounds &real_vec_bounds, unsigned i, double val) {
+    real_vec_bounds[i]->truncate(val);
+    return val;
+};
+
+void vec_truncateWrap2(eoRealVectorBounds &real_vec_bounds, np::ndarray vec) {
+    double* arr = reinterpret_cast<double*>(vec.get_data());
+
+    for(unsigned i =0;i<real_vec_bounds.size();i++){
+        real_vec_bounds[i]->truncate(arr[i]);
+    }
+};
+
+
+
+//eoRealNoBounds
+//eoRealInterval
+//eoRealBelowBound
+//eoRealAboveBound
+template <class RealBounds, class Init>
+void add_real_bounds(std::string name, Init init)
+{
+    using namespace boost::python;
+
+    class_<RealBounds, bases<eoRealBounds>>(name.c_str(), init) \
+        .def("isBounded", &RealBounds::isBounded)
+        .def("hasNoBoundAtAll", &RealBounds::hasNoBoundAtAll)
+        .def("isMinBounded", &RealBounds::isMinBounded)
+        .def("isMaxBounded", &RealBounds::isMaxBounded)
+        .def("isInBounds", &RealBounds::isInBounds)
+        .def("foldsInBounds",foldsInBoundsWrap)
+        .def("truncate", truncateWrap)
+        .def("minimum", &RealBounds::minimum)
+        .def("maximum", &RealBounds::maximum)
+        .def("range", &RealBounds::range)
+        ;
+}
+
+
+
+
+
+#define DEF_FUN(x) .def("isBounded", &x::isBounded) \
+    .def("hasNoBoundAtAll", &x::hasNoBoundAtAll) \
+    .def("isMinBounded", &x::isMinBounded) \
+    .def("isMaxBounded", &x::isMaxBounded) \
+    .def("isInBounds", &x::isInBounds) \
+    .def("foldsInBounds",foldsInBoundsWrap) \
+    .def("truncate", truncateWrap) \
+    .def("minimum", &x::minimum) \
+    .def("maximum", &x::maximum) \
+    .def("range", &x::range) \
+    ;
+
+#define DEF0(x) class_<x, bases<eoRealBounds>>(#x, init<>()) DEF_FUN(x)
+
+#define DEF1(x,i1) class_<x, bases<eoRealBounds>>(#x, init<i1>()) DEF_FUN(x)
+
+#define DEF2(x,i1,i2) class_<x, bases<eoRealBounds>>(#x, init<i1,i2>()) DEF_FUN(x)
+
+
+void uniformWrap(eoRealBaseVectorBounds& _b, np::ndarray _v, eoRng & _rng = eo::rng)
+{
+    double* vec = reinterpret_cast<double*>(_v.get_data());
+
+    for (unsigned i=0; i<_b.size(); i++)
+    {
+        vec[i] = _b.uniform(i, _rng);
+    }
+}
+
+
+
+//overload constructor for bounds...?
+// static std::shared_ptr<eoRealVectorBounds> makeClass()
+
+// BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(unif_overload, uniform, 0, 1)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(uniform1_overload, uniform, 1, 2)
+BOOST_PYTHON_FUNCTION_OVERLOADS(uniform2_overload, uniformWrap, 2, 3)
+
+// BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(uniform1_overload, static_cast<double (eoRealBaseVectorBounds::*)(unsigned,eoRng&)>(&eoRealBaseVectorBounds::uniform), 1, 2)
+
+
 void
 bounds()
 {
+    using namespace boost::python;
+
     class_<eoRealBoundsWrap, boost::noncopyable>("RealBounds",
       init<>())
     .def("isBounded", pure_virtual(&eoRealBoundsWrap::isBounded))
     .def("hasNoBoundAtAll", pure_virtual(&eoRealBoundsWrap::hasNoBoundAtAll))
     .def("isMinBounded", pure_virtual(&eoRealBoundsWrap::isMinBounded))
     .def("isMaxBounded", pure_virtual(&eoRealBoundsWrap::isMaxBounded))
-    .def("isInBounds", pure_virtual(&eoRealBoundsWrap::isBounded))
+    .def("isInBounds", pure_virtual(&eoRealBoundsWrap::isInBounds))
+    .def("foldsInBounds", pure_virtual(&eoRealBoundsWrap::foldsInBounds))
     .def("truncate", pure_virtual(&eoRealBoundsWrap::truncate))
     .def("minimum", pure_virtual(&eoRealBoundsWrap::minimum))
     .def("maximum", pure_virtual(&eoRealBoundsWrap::maximum))
+    .def("range", pure_virtual(&eoRealBoundsWrap::range))
+    .def("uniform", pure_virtual(&eoRealBoundsWrap::uniform))
     ;
+
+
+    ////////// derived from RealBounds
+    //////////////////////////////////////////////////
+    add_real_bounds<eoRealNoBounds>("RealNoBounds",init<>());
+    add_real_bounds<eoRealInterval>("RealInterval",init<double,double>());
+    add_real_bounds<eoRealBelowBound>("RealBelowBound",init<double>());
+    add_real_bounds<eoRealAboveBound>("RealAboveBound",init<double>());
+    // DEF0(eoRealNoBounds)
+    // DEF2(eoRealInterval,double,double)
+    // DEF1(eoRealBelowBound,double)
+    // DEF1(eoRealAboveBound,double)
+
 
     // expose do derive eoRealBaseVectorBounds
     class_<std::vector<eoRealBounds *> >("BoundsVector")
@@ -127,21 +259,14 @@ bounds()
     ;
 
 
-
     bool (eoRealBaseVectorBounds::* isBounded1) (unsigned) = &eoRealBaseVectorBounds::isBounded;
     bool (eoRealBaseVectorBounds::* isBounded2) (void)     = &eoRealBaseVectorBounds::isBounded;
-    bool (eoRealBaseVectorBounds::* hasNoBoundAtAll1) (unsigned) = &eoRealBaseVectorBounds::isBounded;
-    bool (eoRealBaseVectorBounds::* hasNoBoundAtAll2) (void) = &eoRealBaseVectorBounds::isBounded;
-    void (eoRealBaseVectorBounds::* foldsInBounds1) (unsigned, double&) = &eoRealBaseVectorBounds::foldsInBounds;
-    void (eoRealBaseVectorBounds::* foldsInBounds2) (std::vector<double> &) = &eoRealBaseVectorBounds::foldsInBounds;
-    void (eoRealBaseVectorBounds::* truncated1) (unsigned, double&) = &eoRealBaseVectorBounds::truncate;
-    void (eoRealBaseVectorBounds::* truncated2) (std::vector<double> &) = &eoRealBaseVectorBounds::truncate;
+    bool (eoRealBaseVectorBounds::* hasNoBoundAtAll1) (unsigned) = &eoRealBaseVectorBounds::hasNoBoundAtAll;
+    bool (eoRealBaseVectorBounds::* hasNoBoundAtAll2) (void) = &eoRealBaseVectorBounds::hasNoBoundAtAll;
     bool (eoRealBaseVectorBounds::* isInBounds1) (unsigned, double) = &eoRealBaseVectorBounds::isInBounds;
     bool (eoRealBaseVectorBounds::* isInBounds2) (const std::vector<double>) = &eoRealBaseVectorBounds::isInBounds;
 
-    //TODO overload !
-    // double (eoRealBaseVectorBounds::* uniform1) (unsigned,eoRng&) = &eoRealBaseVectorBounds::uniform;
-    // void (eoRealBaseVectorBounds::* uniform2) (std:vector<double>&,eoRng&) = &eoRealBaseVectorBounds::uniform;
+
 
 
     class_<eoRealBaseVectorBounds, bases<std::vector<eoRealBounds *> > >("RealBaseVectorBounds", init<>())
@@ -159,21 +284,30 @@ bounds()
     .def("hasNoBoundAtAll", hasNoBoundAtAll2)
     .def("isMinBounded",&eoRealBaseVectorBounds::isMinBounded)
     .def("isMaxBounded",&eoRealBaseVectorBounds::isMaxBounded)
-    .def("foldsInBounds",foldsInBounds1)
-    .def("foldsInBounds",foldsInBounds2)
-    .def("truncated",truncated1)
-    .def("truncated",truncated2)
+    .def("foldsInBounds",vec_foldsInBoundsWrap)
+    .def("foldsInBounds",vec_foldsInBoundsWrap2)
+    .def("truncate",vec_truncateWrap)
+    .def("truncate",vec_truncateWrap2)
     .def("isInBounds",isInBounds1)
     .def("isInBounds",isInBounds2)
     .def("minimum",&eoRealBaseVectorBounds::minimum)
     .def("maximum",&eoRealBaseVectorBounds::maximum)
     .def("range",&eoRealBaseVectorBounds::range)
     .def("averageRange",&eoRealBaseVectorBounds::averageRange)
-    // .def("uniform",uniform1)
-    // .def("uniform",uniform2)
+    .def("uniform", static_cast<double (eoRealBaseVectorBounds::*)(unsigned,eoRng&)>(&eoRealBaseVectorBounds::uniform),uniform1_overload(args("i","_rng"))
+    )
+    .def("uniform", uniformWrap, uniform2_overload())
     ;
 
+
+    //ATTENTION : order of constructors matters!
+    // https://stackoverflow.com/questions/8140155/boost-python-confused-about-similar-constructor
     class_<eoRealVectorBounds, bases<eoRealBaseVectorBounds> >("RealVectorBounds", init<>())
+    .def(init<
+          std::vector<double>,
+          std::vector<double>
+      >()
+    )
     .def(init<
           unsigned,
           eoRealBounds&
