eb6773d81f6a6c8ec9c207c5e8fa6e157554b255
GMYS Jan <jan.gmys@inria.fr>
2022-02-03 18:40:47 +0100

    test binary solution initializer

---
+++ tests/test_sol_binary.py
@@ -0,0 +1,26 @@
+from pyparadiseo import PyEOT
+
+from pyparadiseo import BinaryInit
+
+import numpy as np
+
+import unittest
+
+class TestBinary(unittest.TestCase):
+    def setUp(self):
+        self.sol = PyEOT()
+
+    def test_len(self):
+        init = BinaryInit(10)
+        init(self.sol)
+        self.assertEqual(len(self.sol),10)
+
+    def test_iter(self):
+        init = BinaryInit(10)
+        init(self.sol)
+        #check that we can iterate solution and it contains bools
+        for i in self.sol:
+            self.assertTrue(isinstance(i, np.bool_))
+
+if __name__ == '__main__':
+    unittest.main()
