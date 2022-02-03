e3d73a3d6634e6042478dd1eb3138f99f5f8b054
GMYS Jan <jan.gmys@inria.fr>
2022-02-03 18:23:30 +0100

    example problems

---
+++ examples/problems/onemax.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class OneMax():
+    def __init__(self,_dim):
+        self.dim = _dim
+        pass
+
+    def sum_bits(self,sol) -> float:
+        return float(np.count_nonzero(sol))
+
+    def eval_incremental(self,sol_enc,sol_fit,index) -> float:
+        if sol_enc[index]:
+            return sol_fit - 1
+        else:
+            return sol_fit + 1
+
+    def move(self,nbor,sol):
+        ind = nbor.index()
+        if sol[ind]:
+            sol[ind] = False
+        else:
+            sol[ind] = True
+
+
+# def sum_bits(sol):
+#     return np.count_nonzero(sol)
+
+# def move(self,nbor,sol):
+#     ind = nbor.index()
+#     if sol[ind]:
+#         sol[ind] = False
+#     else:
+#         sol[ind] = True
+#
+# def evalIncremental(sol_enc,sol_fit,index) -> float:
+#     if sol_enc[index]:
+#         return sol_fit - 1
+#     else:
+#         return sol_fit + 1
---
+++ examples/problems/tsp.py
@@ -0,0 +1,95 @@
+# from pyparadiseo.problem.problem import PermutationProblem
+
+import os
+import numpy as np
+#import numba
+import math
+
+class TravelingSalesman():
+    def __init__(self, instance : str = None):
+        self.ncities,self.xcoord,self.ycoord = self.read_input(instance)
+
+        self.moves = []
+        for i in range(1,self.ncities):
+            for j in range(i+1,self.ncities):
+                self.moves.append((i,j))
+
+    def read_input(self,instance_name : str):
+        """Read TSP instance from ./problemData/tsp/{instance_name}.tsp
+
+        Parameters
+        ----------
+        instance_name : str
+            name of the file to read, without .tsp extension
+
+        Returns
+        -------
+
+        """
+        if instance_name is None:
+            raise FileNotFoundError('no file to read from')
+
+        filename=os.path.dirname(__file__)+"/problemData/tsp/"+instance_name+".tsp"
+        print("[] Reading instance data from ",filename)
+
+        with open(filename, 'r') as f:
+            content = f.read()
+            lines = content.split('\n')
+
+            ncities = int(lines[3].split()[-1])
+
+            xcoord = np.zeros(ncities,dtype=float)
+            ycoord = np.zeros(ncities,dtype=float)
+
+            for i,line in enumerate(lines[6:]):
+                if line.startswith('EOF'):
+                    break
+                else:
+                    X, Y = line.split()[1:]
+                    xcoord[i]=X
+                    ycoord[i]=Y
+
+        return ncities,xcoord,ycoord
+
+    def get_edge_length(self, a, b):
+        dx = self.xcoord[a] - self.xcoord[b]
+        dy = self.ycoord[a] - self.ycoord[b]
+        return math.sqrt(dx * dx + dy * dy)
+
+    ### need this
+    def eval(self,perm):
+        cost = self.get_edge_length(0, perm[1])
+        for i in range(1,len(perm)-1):
+            cost += self.get_edge_length(perm[i], perm[i + 1])
+        cost += self.get_edge_length(perm[len(perm) - 1], 0)
+        return cost
+
+    ### need that (not == Permutation because tour[0] = 0)
+    def make_random_tour(self):
+        tour = np.arange(self.ncities)
+        np.random.shuffle(tour[1:])
+        return tour
+
+    def eval_incremental(self,sol,sol_fitness,index):
+        a,b=self.moves[index]
+        tourlen = sol_fitness
+        tourlen -= self.get_edge_length(sol[a-1],sol[a])
+        tourlen += self.get_edge_length(sol[a-1],sol[b])
+        if b == self.ncities-1:
+            tourlen -= self.get_edge_length(sol[b],sol[0])
+            tourlen += self.get_edge_length(sol[b],sol[0])
+        else:
+            tourlen -= self.get_edge_length(sol[b],sol[b+1])
+            tourlen += self.get_edge_length(sol[a],sol[b+1])
+
+        # neigh.fitness = tourlen
+        return tourlen
+
+    def move(self,nbor,sol):
+        """assuming i<j"""
+        i,j=self.moves[nbor.index()]
+        while i<j:
+            sol[i],sol[j]=sol[j],sol[i]
+            i += 1
+            j -= 1
+        return sol
