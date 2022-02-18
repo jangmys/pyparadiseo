bd2841399a11677da5bc71e90d64e4a01433139a
GMYS Jan <jan.gmys@inria.fr>
2022-02-18 21:44:40 +0100

    fix import error

--- src/pyparadiseo/__init__.py
+++ src/pyparadiseo/__init__.py
@@ -5,17 +5,17 @@ from .initializer import *
 from .evaluator import *
 from .bounds import *
 
-from .core import *
+from ._core import *
 
 from ._core import FitnessTraits
 
 ### basic stuff common to MO,EO and MOEO
 
 #SOLUTION (PyEOT)
-from .core import Solution
-from .core import Pop
-from .core import ObjectiveVectorTraits
-from .core import ObjectiveVector
+from ._core import Solution
+from ._core import Pop
+from ._core import ObjectiveVectorTraits
+from ._core import ObjectiveVector
 
 #  EO RNG
 from ._core import eoRng
